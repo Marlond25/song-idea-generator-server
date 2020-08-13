2a27236ac5170dc4121e2b133117c95e2da5451d
Marlon ryzen <enginer26mj@gmail.com>
2020-08-12 22:38:10 -0500

    Added cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors require('cors');
 const mongoose = require('mongoose');
 
 const instruments = require('./src/api/instruments');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 mongoose
